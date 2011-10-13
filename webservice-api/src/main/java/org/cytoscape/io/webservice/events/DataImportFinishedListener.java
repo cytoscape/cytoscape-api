e7ffbe9ca7e27bbe37672cb9c3cc1cf34737c004
mkirby <mkirby@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-10-13 07:09:41 +0000

    Fixed Javadoc.

--- webservice-api/src/main/java/org/cytoscape/io/webservice/events/DataImportFinishedListener.java
+++ webservice-api/src/main/java/org/cytoscape/io/webservice/events/DataImportFinishedListener.java
@@ -1,5 +1,8 @@
 package org.cytoscape.io.webservice.events;
 
+/**
+ * Listener for {@link DataImportFinishedEvent}.
+ */
 public interface DataImportFinishedListener {
 	void handleEvent(DataImportFinishedEvent<?> evt);
 }
--- webservice-api/src/main/java/org/cytoscape/io/webservice/events/SearchFinishedListener.java
+++ webservice-api/src/main/java/org/cytoscape/io/webservice/events/SearchFinishedListener.java
@@ -1,5 +1,8 @@
 package org.cytoscape.io.webservice.events;
 
+/**
+ * Listener for {@link SearchFinishedEvent}.
+ */
 public interface SearchFinishedListener {
 	void handleEvent(final SearchFinishedEvent evt);
 }
