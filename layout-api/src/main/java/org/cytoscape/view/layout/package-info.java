040737957d663e45ecdda6a3921d701543d11143
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-11-28 22:29:38 +0000

    Original creation

---
+++ layout-api/src/main/java/org/cytoscape/view/layout/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package provides access to the available layout algorithms. It also provides abstract layout 
+ * classes and layout information containers for the convenience of implementing other layout algorithms.
+ */
+package org.cytoscape.view.layout;
