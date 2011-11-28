f2bfebda82ff540f7040477eb20eac9a8e8833bb
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-11-28 23:58:09 +0000

    Original creation

---
+++ vizmap-gui-api/src/main/java/org/cytoscape/view/vizmap/gui/event/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides events and listeners of LexiconState, visual style switch and vizMap event, as well as VizMap event handler manager.
+ */
+package org.cytoscape.view.vizmap.gui.event;
