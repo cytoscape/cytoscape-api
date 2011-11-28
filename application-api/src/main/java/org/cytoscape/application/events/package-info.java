597d3e10dc48c2200ea525baeb4acce7562de05d
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-11-28 20:18:36 +0000

    Original creation

---
+++ application-api/src/main/java/org/cytoscape/application/events/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides application events/listeners, including 
+ * Cytoscape startup/shutdown, setCurrentNetwork/setCurrentNetworkView/
+ * setSelectedNetwork.
+ */
+package org.cytoscape.application.events;
