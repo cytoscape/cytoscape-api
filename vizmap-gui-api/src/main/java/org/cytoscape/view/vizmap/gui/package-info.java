95935ef9d1e9edd3f72dd2383d9a1d90db79da5c
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-11-29 00:03:29 +0000

    text updated

--- vizmap-gui-api/src/main/java/org/cytoscape/view/vizmap/gui/package-info.java
+++ vizmap-gui-api/src/main/java/org/cytoscape/view/vizmap/gui/package-info.java
@@ -1,5 +1,5 @@
 /**
- * This package Provides access to GUI component of VizMap GUI, and interfaces of mapping related managers.
+ * This package provides access to GUI component of VizMap GUI, and interfaces of mapping related managers.
 
  */
 package org.cytoscape.view.vizmap.gui;
