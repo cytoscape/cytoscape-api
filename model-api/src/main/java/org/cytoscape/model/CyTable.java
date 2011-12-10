3e4aa748c4e894ae7e49a6cc61212014b0720118
apico <apico@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-12-10 00:16:27 +0000

    Major change from Plugin to App!

--- model-api/src/main/java/org/cytoscape/model/CyTable.java
+++ model-api/src/main/java/org/cytoscape/model/CyTable.java
@@ -83,7 +83,7 @@ public interface CyTable extends Identifiable {
 	/**
 	 * A public CyTable is a table that is accessible to the user through the user
 	 * interface.  Private or non-public CyTables will not be visible to the user from the
-	 * normal user interface, although they will be accessible to plugin writers through the API.
+	 * normal user interface, although they will be accessible to app writers through the API.
 	 *
 	 * @return Whether or not this CyTable should be publicly accessible.
 	 */
