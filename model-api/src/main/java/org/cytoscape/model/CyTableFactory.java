b4b09797ca7799527d28e1096a669b9a86e05ff2
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-01-25 01:27:58 +0000

    fixes #591 we are now a bit smarter about the size of tables that we create by default, which was the root of the time and memory consumption problems here.

--- model-api/src/main/java/org/cytoscape/model/CyTableFactory.java
+++ model-api/src/main/java/org/cytoscape/model/CyTableFactory.java
@@ -35,6 +35,48 @@ package org.cytoscape.model;
  * @CyAPI.Api.Interface
  */
 public interface CyTableFactory {
+	/** 
+	 * A description of the initial size of the table.
+	 * Used only for the initial construction of the table
+	 * and does not in any way limit the eventual size
+	 * of the table!
+	 */
+	enum InitialTableSize {
+		/** Small - about 100 entries. */
+		SMALL(100),
+		/** Medium - about 1000 entries. */
+		MEDIUM(1000),
+		/** Large - about 10,000 entries. */
+		LARGE(10000),
+		;
+
+		private final int size; 
+		private InitialTableSize(int size) {
+			this.size = size;
+		}
+
+		/**
+		 * Returns the actual size value for the given enum.
+		 * @return the actual size value for the given enum.
+		 */
+		public int getSize() {
+			return size;
+		}
+	}
+
+	/**
+	 * Creates a CyTable object with the specified name, primary key, visibility, and mutability.
+	 * @param title The name of the CyTable.
+	 * @param primaryKey The name primaryKey column for this table. 
+	 * @param primaryKeyType The type of the primaryKey column for this table. 
+	 * @param pub Whether or not the CyTable should be public.
+	 * @param isMutable if true, the table can be deleted later on, otherwise it can't
+	 * @param initialSize a rough guess as to the expected size of the table 
+	 * @return A new {@link CyTable} with the specified name that is either public or not (see
+	 *         {@link CyTable#isPublic}.
+	 */
+	CyTable createTable(String title, String primaryKey, Class<?> primaryKeyType, boolean pub,
+			    boolean isMutable, InitialTableSize initialSize);
 	/**
 	 * Creates a CyTable object with the specified name, primary key, visibility, and mutability.
 	 * @param title The name of the CyTable.
