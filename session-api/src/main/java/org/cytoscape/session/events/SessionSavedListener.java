53c5089ecb03f0af05ebf2f64adc42952f142e11
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-02-27 21:28:03 +0000

    fixes #699 SassionSavedEvent and Listener had been added to solve this issue.

---
+++ session-api/src/main/java/org/cytoscape/session/events/SessionSavedListener.java
@@ -0,0 +1,16 @@
+package org.cytoscape.session.events;
+
+import org.cytoscape.event.CyListener;
+
+/**
+ * A listener for {@link SessionSavedEvent}s.
+ * @CyAPI.Spi.Interface
+ */
+public interface SessionSavedListener extends CyListener {
+	
+	/**
+	 * Processes the specified event when fired.
+	 * @param e The event that the listener is listening for.
+	 */
+	public void handleEvent(final SessionSavedEvent e);
+}
