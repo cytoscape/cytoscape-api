35b4b4814782b88cb4bb304141a5b4e261404e0a
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-03-23 19:19:54 +0000

    Hard-coded property keys had been moved to this interface.

---
+++ work-api/src/main/java/org/cytoscape/work/ServiceProperties.java
@@ -0,0 +1,16 @@
+package org.cytoscape.work;
+
+/**
+ * Reserved key words for service properties.
+ * These properties will be used for {@linkplain TaskFactory} services.
+ */
+public interface ServiceProperties {
+	// TODO Move all property documentations here.
+	
+	// Reserved Key Words
+	public static final String ENABLE_FOR = "enableFor";
+	public static final String TITLE = "title";
+	public static final String PREFERRED_MENU = "preferredMenu";
+	public static final String COMMAND = "command";
+	public static final String COMMAND_NAMESPACE = "commandNamespace";
+}
