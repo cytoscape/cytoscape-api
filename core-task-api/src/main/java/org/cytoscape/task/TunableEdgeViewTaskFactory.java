e5ffd1866675ff1b85a203ec9a40e96e7879d299
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-17 20:09:17 +0000

    fixes #881 removed unused TunableNodeView and TunableEdgeView task factories

--- core-task-api/src/main/java/org/cytoscape/task/TunableEdgeViewTaskFactory.java
+++
@@ -1,9 +0,0 @@
-package org.cytoscape.task;
-
-import org.cytoscape.model.CyEdge;
-import org.cytoscape.view.model.CyNetworkView;
-import org.cytoscape.view.model.View;
-
-public interface TunableEdgeViewTaskFactory<T> extends EdgeViewTaskFactory {
-	T createTunableContext(View<CyEdge> edgeView, CyNetworkView networkView);
-}
