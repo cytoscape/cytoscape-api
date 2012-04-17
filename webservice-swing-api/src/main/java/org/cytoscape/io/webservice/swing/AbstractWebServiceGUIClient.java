7576fdd6a239dd1bfddb50bec58b2225666550f1
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-17 22:52:36 +0000

    updated javadocs

--- webservice-swing-api/src/main/java/org/cytoscape/io/webservice/swing/AbstractWebServiceGUIClient.java
+++ webservice-swing-api/src/main/java/org/cytoscape/io/webservice/swing/AbstractWebServiceGUIClient.java
@@ -6,13 +6,18 @@ import org.cytoscape.io.webservice.client.AbstractWebServiceClient;
 
 /**
  * Abstract base class for web service clients with Swing GUI.
- *
  */
 public abstract class AbstractWebServiceGUIClient extends AbstractWebServiceClient implements WebServiceGUIClient {
 
 	// Client GUI.  If this is null, default GUI will be used. 
 	protected Container gui;
 
+	/**
+	 * Constructor.
+	 * @param uri A String representation of the URI.
+	 * @param displayName The display name to be used for this client.
+	 * @param description The description to be used for this client.
+	 */
 	public AbstractWebServiceGUIClient(final String uri, final String displayName, final String description) {
 		super(uri, displayName, description);
 	}
--- webservice-swing-api/src/main/java/org/cytoscape/io/webservice/swing/WebServiceGUIClient.java
+++ webservice-swing-api/src/main/java/org/cytoscape/io/webservice/swing/WebServiceGUIClient.java
@@ -2,11 +2,15 @@ package org.cytoscape.io.webservice.swing;
 
 import java.awt.Container;
 
+/** 
+ * An interface that allows web service clients to provide
+ * their own GUI component.
+ */
 public interface WebServiceGUIClient {
+
 	/**
 	 * Returns query builder UI. Since this is a TaskFactory,
 	 * createTaskIterator() method should use parameters from this GUI.
-	 * 
 	 * @return query builder UI.
 	 */
 	Container getQueryBuilderGUI();
