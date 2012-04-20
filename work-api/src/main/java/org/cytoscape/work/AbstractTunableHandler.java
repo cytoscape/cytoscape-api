1d83d3ffd436f049678b4bda021998252400bce4
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-20 21:50:52 +0000

    Import statements had been reorganized.

--- work-api/src/main/java/org/cytoscape/work/AbstractTunableHandler.java
+++ work-api/src/main/java/org/cytoscape/work/AbstractTunableHandler.java
@@ -4,9 +4,6 @@ package org.cytoscape.work;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 
