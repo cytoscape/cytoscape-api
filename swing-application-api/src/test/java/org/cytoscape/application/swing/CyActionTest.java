3d8f050bbb65cc9d91b55c5a304504be48b56d5d
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-21 00:03:12 +0000

    fixes #868 New unit tests had been added to swing-application-api.

--- swing-application-api/src/test/java/org/cytoscape/application/swing/CyActionTest.java
+++ swing-application-api/src/test/java/org/cytoscape/application/swing/CyActionTest.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.view.model.CyNetworkViewManager;
+import org.cytoscape.work.ServiceProperties;
 import org.cytoscape.work.TaskFactory;
 import org.junit.After;
 import org.junit.Before;
@@ -21,6 +22,20 @@ public class CyActionTest extends AbstractCyActionTest {
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 
+		configProps.put(ServiceProperties.PREFERRED_MENU, "File");
+		configProps.put(ServiceProperties.ACCELERATOR, "cmd s");
+		configProps.put(ServiceProperties.COMMAND, "test");
+		configProps.put(ServiceProperties.COMMAND_NAMESPACE, "dummy");
+		configProps.put(ServiceProperties.ENABLE_FOR, StringEnableSupport.ENABLE_FOR_NETWORK);
+		configProps.put(ServiceProperties.ID, "dummyAction");
+		configProps.put(ServiceProperties.IN_MENU_BAR, "true");
+		configProps.put(ServiceProperties.IN_TOOL_BAR, "false");
+		configProps.put(ServiceProperties.LARGE_ICON_URL, "test.png");
+		configProps.put(ServiceProperties.SMALL_ICON_URL, "small.png");
+		configProps.put(ServiceProperties.MENU_GRAVITY, "10.0");
+		configProps.put(ServiceProperties.TITLE, "Dummy Cy Action");
+		configProps.put(ServiceProperties.TOOL_BAR_GRAVITY, "200");
+		configProps.put(ServiceProperties.TOOLTIP, "This is a dummy action.");
 		action = new DummyAction(configProps, applicationManager, networkViewManager);
 		action.setName(name);
 	}
