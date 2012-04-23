204174ed9a06b13616f350c74c1db19f23ca489a
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-23 23:42:44 +0000

    some updates to core-task-api, cleanup of getCurrentNetwork and removal of the unused quickstart code

--- core-task-api/src/main/java/org/cytoscape/task/edit/ConnectSelectedNodesTaskFactory.java
+++ core-task-api/src/main/java/org/cytoscape/task/edit/ConnectSelectedNodesTaskFactory.java
@@ -1,11 +1,11 @@
 package org.cytoscape.task.edit;
 
-import org.cytoscape.work.TaskFactory;
+import org.cytoscape.task.NetworkTaskFactory;
 
 /**
  * This interface provides a task iterator for connecting selected nodes.
  * @CyAPI.Api.Interface
  */
-public interface ConnectSelectedNodesTaskFactory extends TaskFactory {
+public interface ConnectSelectedNodesTaskFactory extends NetworkTaskFactory {
 
 }
--- core-task-api/src/main/java/org/cytoscape/task/networkobjects/DeleteSelectedNodesAndEdgesTaskFactory.java
+++ core-task-api/src/main/java/org/cytoscape/task/networkobjects/DeleteSelectedNodesAndEdgesTaskFactory.java
@@ -1,12 +1,12 @@
 package org.cytoscape.task.networkobjects;
 
-import org.cytoscape.work.TaskFactory;
+import org.cytoscape.task.NetworkTaskFactory;
 
 /**
  * This interface provides a task iterator for deleting selected nodes and edges.
  * @CyAPI.Api.Interface
  *
  */
-public interface DeleteSelectedNodesAndEdgesTaskFactory extends TaskFactory{
+public interface DeleteSelectedNodesAndEdgesTaskFactory extends NetworkTaskFactory{
 
 }
