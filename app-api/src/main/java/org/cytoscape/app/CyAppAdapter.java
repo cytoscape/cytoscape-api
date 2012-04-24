a1248135bad06c7f25427b044362435989a3eb55
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-24 18:56:40 +0000

    Fixed bug#882

--- app-api/src/main/java/org/cytoscape/app/CyAppAdapter.java
+++ app-api/src/main/java/org/cytoscape/app/CyAppAdapter.java
@@ -1,7 +1,6 @@
 package org.cytoscape.app;
 
 import java.util.Properties;
-
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.application.CyVersion;
 import org.cytoscape.application.swing.CySwingApplication;
@@ -35,6 +34,56 @@ import org.cytoscape.work.swing.DialogTaskManager;
 import org.cytoscape.work.swing.PanelTaskManager;
 import org.cytoscape.work.swing.SubmenuTaskManager;
 import org.cytoscape.work.undo.UndoSupport;
+import org.cytoscape.task.table.MapGlobalToLocalTableTaskFactory;
+import org.cytoscape.task.vizmap.ApplyVisualStyleTaskFactory;
+import org.cytoscape.task.layout.ApplyPreferredLayoutTaskFactory;
+import org.cytoscape.task.loadnetwork.LoadNetworkFileTaskFactory;
+import org.cytoscape.task.loadnetwork.LoadNetworkURLTaskFactory;
+import org.cytoscape.task.loadvizmap.LoadVizmapFileTaskFactory;
+import org.cytoscape.task.loaddatatable.LoadAttributesFileTaskFactory;
+import org.cytoscape.task.loaddatatable.LoadAttributesURLTaskFactory;
+import org.cytoscape.task.networkobjects.DeleteSelectedNodesAndEdgesTaskFactory;
+import org.cytoscape.task.select.InvertSelectedEdgesTaskFactory;
+import org.cytoscape.task.select.InvertSelectedNodesTaskFactory;
+import org.cytoscape.task.select.SelectAdjacentEdgesTaskFactory;
+import org.cytoscape.task.select.SelectAllTaskFactory;
+import org.cytoscape.task.select.SelectAllEdgesTaskFactory;
+import org.cytoscape.task.select.SelectAllNodesTaskFactory;
+import org.cytoscape.task.select.SelectFirstNeighborsNodeViewTaskFactory;
+import org.cytoscape.task.select.SelectFirstNeighborsTaskFactory;
+import org.cytoscape.task.select.SelectConnectedNodesTaskFactory;
+import org.cytoscape.task.select.DeselectAllTaskFactory;
+import org.cytoscape.task.select.DeselectAllEdgesTaskFactory;
+import org.cytoscape.task.select.DeselectAllNodesTaskFactory;
+import org.cytoscape.task.select.SelectFromFileListTaskFactory;
+import org.cytoscape.task.hide.HideSelectedTaskFactory;
+import org.cytoscape.task.hide.HideSelectedNodesTaskFactory;
+import org.cytoscape.task.hide.HideSelectedEdgesTaskFactory;
+import org.cytoscape.task.hide.UnHideAllTaskFactory;
+import org.cytoscape.task.hide.UnHideAllNodesTaskFactory;
+import org.cytoscape.task.hide.UnHideAllEdgesTaskFactory;
+import org.cytoscape.task.creation.NewEmptyNetworkViewFactory;
+import org.cytoscape.task.creation.NewNetworkSelectedNodesAndEdgesTaskFatory;
+import org.cytoscape.task.creation.NewNetworkSelectedNodesOnlyTaskFactory;
+import org.cytoscape.task.creation.CloneNetworkTaskFactory;
+import org.cytoscape.task.destruction.DestroyNetworkTaskFactory;
+import org.cytoscape.task.destruction.DestroyNetworkViewTaskFactory;
+import org.cytoscape.task.NetworkViewTaskFactory;
+import org.cytoscape.task.title.EditNetworkTitleTaskFactory;
+import org.cytoscape.task.creation.CreateNetworkViewTaskFactory;
+import org.cytoscape.task.edit.ConnectSelectedNodesTaskFactory;
+import org.cytoscape.task.export.graphics.ExportNetworkImageTaskFactory;
+import org.cytoscape.task.export.network.ExportNetworkViewTaskFactory;
+import org.cytoscape.task.export.table.ExportCurrentTableTaskFactory;
+import org.cytoscape.task.export.vizmap.ExportVizmapTaskFactory;
+import org.cytoscape.task.session.NewSessionTaskFactory;
+import org.cytoscape.task.session.OpenSessionTaskFactory;
+import org.cytoscape.task.session.SaveSessionAsTaskFactory;
+import org.cytoscape.task.table.DeleteColumnTaskFactory;
+import org.cytoscape.task.table.DeleteTableTaskFactory;
+import org.cytoscape.task.table.RenameColumnTaskFactory;
+import org.cytoscape.task.table.MapNetworkAttrTaskFactory;
+
 
 /**
  * A Java-only api providing access to Cytoscape functionality.
@@ -313,4 +362,375 @@ public interface CyAppAdapter {
 	 * @return an instance of {@link DataSourceManager}.
 	 */
 	DataSourceManager getDataSourceManager();
+
+	
+	//////////////////////////// core-task API ///////////////////////////////////////////////////////////
+
+	// core-task API
+	
+	/**
+	 * Returns an instance of {@link ApplyVisualStyleTaskFactory}.
+	 * @return an instance of {@link ApplyVisualStyleTaskFactory}.
+	 */
+	ApplyVisualStyleTaskFactory get_ApplyVisualStyleTaskFactory();
+	
+	
+	/**
+	 * Returns an instance of {@link MapGlobalToLocalTableTaskFactory}.
+	 * @return an instance of {@link MapGlobalToLocalTableTaskFactory}.
+	 */
+	MapGlobalToLocalTableTaskFactory get_MapGlobalToLocalTableTaskFactory();
+	
+	/**
+	 * Returns an instance of {@link LoadNetworkFileTaskFactory}.
+	 * @return an instance of {@link LoadNetworkFileTaskFactory}.
+	 */
+	LoadNetworkFileTaskFactory get_LoadNetworkFileTaskFactory();
+
+	/**
+	 * Returns an instance of {@link LoadNetworkURLTaskFactory}.
+	 * @return an instance of {@link LoadNetworkURLTaskFactory}.
+	 */
+	LoadNetworkURLTaskFactory get_LoadNetworkURLTaskFactory();
+
+	
+	/**
+	 * Returns an instance of {@link LoadVizmapFileTaskFactory}.
+	 * @return an instance of {@link LoadVizmapFileTaskFactory}.
+	 */
+	LoadVizmapFileTaskFactory get_LoadVizmapFileTaskFactory();
+
+	/**
+	 * Returns an instance of {@link LoadAttributesFileTaskFactory}.
+	 * @return an instance of {@link LoadAttributesFileTaskFactory}.
+	 */
+	LoadAttributesFileTaskFactory get_LoadAttributesFileTaskFactory();
+
+	/**
+	 * Returns an instance of {@link LoadAttributesURLTaskFactory}.
+	 * @return an instance of {@link LoadAttributesURLTaskFactory}.
+	 */
+	LoadAttributesURLTaskFactory get_LoadAttributesURLTaskFactory();
+
+	/**
+	 * Returns an instance of {@link DeleteSelectedNodesAndEdgesTaskFactory}.
+	 * @return an instance of {@link DeleteSelectedNodesAndEdgesTaskFactory}.
+	 */
+	DeleteSelectedNodesAndEdgesTaskFactory get_DeleteSelectedNodesAndEdgesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link SelectAllTaskFactory}.
+	 * @return an instance of {@link SelectAllTaskFactory}.
+	 */
+	SelectAllTaskFactory get_SelectAllTaskFactory();
+
+	/**
+	 * Returns an instance of {@link SelectAllEdgesTaskFactory}.
+	 * @return an instance of {@link SelectAllEdgesTaskFactory}.
+	 */
+	SelectAllEdgesTaskFactory get_SelectAllEdgesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link SelectAllNodesTaskFactory}.
+	 * @return an instance of {@link SelectAllNodesTaskFactory}.
+	 */
+	SelectAllNodesTaskFactory get_SelectAllNodesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link SelectAdjacentEdgesTaskFactory}.
+	 * @return an instance of {@link SelectAdjacentEdgesTaskFactory}.
+	 */
+	SelectAdjacentEdgesTaskFactory get_SelectAdjacentEdgesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link SelectConnectedNodesTaskFactory}.
+	 * @return an instance of {@link SelectConnectedNodesTaskFactory}.
+	 */
+	SelectConnectedNodesTaskFactory get_SelectConnectedNodesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link SelectFirstNeighborsTaskFactory}.
+	 * @return an instance of {@link SelectFirstNeighborsTaskFactory}.
+	 */
+	SelectFirstNeighborsTaskFactory get_SelectFirstNeighborsTaskFactory();
+
+	/**
+	 * Returns an instance of {@link SelectFirstNeighborsTaskFactory}.
+	 * @return an instance of {@link SelectFirstNeighborsTaskFactory}.
+	 */
+	SelectFirstNeighborsTaskFactory get_SelectFirstNeighborsTaskFactoryInEdge();
+
+	/**
+	 * Returns an instance of {@link SelectFirstNeighborsTaskFactory}.
+	 * @return an instance of {@link SelectFirstNeighborsTaskFactory}.
+	 */
+	SelectFirstNeighborsTaskFactory get_SelectFirstNeighborsTaskFactoryOutEdge();
+
+	/**
+	 * Returns an instance of {@link DeselectAllTaskFactory}.
+	 * @return an instance of {@link DeselectAllTaskFactory}.
+	 */
+	DeselectAllTaskFactory get_DeselectAllTaskFactory();
+
+	/**
+	 * Returns an instance of {@link DeselectAllEdgesTaskFactory}.
+	 * @return an instance of {@link DeselectAllEdgesTaskFactory}.
+	 */
+	DeselectAllEdgesTaskFactory get_DeselectAllEdgesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link DeselectAllNodesTaskFactory}.
+	 * @return an instance of {@link DeselectAllNodesTaskFactory}.
+	 */
+	DeselectAllNodesTaskFactory get_DeselectAllNodesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link InvertSelectedEdgesTaskFactory}.
+	 * @return an instance of {@link InvertSelectedEdgesTaskFactory}.
+	 */
+	InvertSelectedEdgesTaskFactory get_InvertSelectedEdgesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link InvertSelectedNodesTaskFactory}.
+	 * @return an instance of {@link InvertSelectedNodesTaskFactory}.
+	 */
+	InvertSelectedNodesTaskFactory get_InvertSelectedNodesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link SelectFromFileListTaskFactory}.
+	 * @return an instance of {@link SelectFromFileListTaskFactory}.
+	 */
+	SelectFromFileListTaskFactory get_SelectFromFileListTaskFactory();
+
+	/**
+	 * Returns an instance of {@link SelectFirstNeighborsNodeViewTaskFactory}.
+	 * @return an instance of {@link SelectFirstNeighborsNodeViewTaskFactory}.
+	 */
+	SelectFirstNeighborsNodeViewTaskFactory get_SelectFirstNeighborsNodeViewTaskFactory();
+
+	/**
+	 * Returns an instance of {@link HideSelectedTaskFactory}.
+	 * @return an instance of {@link HideSelectedTaskFactory}.
+	 */
+	HideSelectedTaskFactory get_HideSelectedTaskFactory();
+
+	/**
+	 * Returns an instance of {@link HideSelectedNodesTaskFactory}.
+	 * @return an instance of {@link HideSelectedNodesTaskFactory}.
+	 */
+	HideSelectedNodesTaskFactory get_HideSelectedNodesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link HideSelectedEdgesTaskFactory}.
+	 * @return an instance of {@link HideSelectedEdgesTaskFactory}.
+	 */
+	HideSelectedEdgesTaskFactory get_HideSelectedEdgesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link UnHideAllTaskFactory}.
+	 * @return an instance of {@link UnHideAllTaskFactory}.
+	 */
+	UnHideAllTaskFactory get_UnHideAllTaskFactory();
+
+	/**
+	 * Returns an instance of {@link UnHideAllNodesTaskFactory}.
+	 * @return an instance of {@link UnHideAllNodesTaskFactory}.
+	 */
+	UnHideAllNodesTaskFactory get_UnHideAllNodesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link UnHideAllEdgesTaskFactory}.
+	 * @return an instance of {@link UnHideAllEdgesTaskFactory}.
+	 */
+	UnHideAllEdgesTaskFactory get_UnHideAllEdgesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link NewEmptyNetworkViewFactory}.
+	 * @return an instance of {@link NewEmptyNetworkViewFactory}.
+	 */
+	NewEmptyNetworkViewFactory get_NewEmptyNetworkViewFactory();
+
+	/**
+	 * Returns an instance of {@link NewNetworkSelectedNodesAndEdgesTaskFatory}.
+	 * @return an instance of {@link NewNetworkSelectedNodesAndEdgesTaskFatory}.
+	 */
+	NewNetworkSelectedNodesAndEdgesTaskFatory get_NewNetworkSelectedNodesAndEdgesTaskFatory();
+
+	/**
+	 * Returns an instance of {@link NewNetworkSelectedNodesOnlyTaskFactory}.
+	 * @return an instance of {@link NewNetworkSelectedNodesOnlyTaskFactory}.
+	 */
+	NewNetworkSelectedNodesOnlyTaskFactory get_NewNetworkSelectedNodesOnlyTaskFactory();
+
+	/**
+	 * Returns an instance of {@link CloneNetworkTaskFactory}.
+	 * @return an instance of {@link CloneNetworkTaskFactory}.
+	 */
+	CloneNetworkTaskFactory get_CloneNetworkTaskFactory();
+
+	/**
+	 * Returns an instance of {@link DestroyNetworkTaskFactory}.
+	 * @return an instance of {@link DestroyNetworkTaskFactory}.
+	 */
+	DestroyNetworkTaskFactory get_DestroyNetworkTaskFactory();
+
+	/**
+	 * Returns an instance of {@link DestroyNetworkViewTaskFactory}.
+	 * @return an instance of {@link DestroyNetworkViewTaskFactory}.
+	 */
+	DestroyNetworkViewTaskFactory get_DestroyNetworkViewTaskFactory();
+
+	/**
+	 * Returns an instance of {@link NetworkViewTaskFactory}.
+	 * @return an instance of {@link NetworkViewTaskFactory}.
+	 */
+	NetworkViewTaskFactory get_ZoomInTaskFactory();
+
+	/**
+	 * Returns an instance of {@link NetworkViewTaskFactory}.
+	 * @return an instance of {@link NetworkViewTaskFactory}.
+	 */
+	NetworkViewTaskFactory get_ZoomOutTaskFactory();
+
+	/**
+	 * Returns an instance of {@link NetworkViewTaskFactory}.
+	 * @return an instance of {@link NetworkViewTaskFactory}.
+	 */
+	NetworkViewTaskFactory get_FitSelectedTaskFactory();
+
+	/**
+	 * Returns an instance of {@link NetworkViewTaskFactory}.
+	 * @return an instance of {@link NetworkViewTaskFactory}.
+	 */
+	NetworkViewTaskFactory get_fitContentTaskFactory();
+
+	/**
+	 * Returns an instance of {@link EditNetworkTitleTaskFactory}.
+	 * @return an instance of {@link EditNetworkTitleTaskFactory}.
+	 */
+	EditNetworkTitleTaskFactory get_EditNetworkTitleTaskFactory();
+
+	/**
+	 * Returns an instance of {@link CreateNetworkViewTaskFactory}.
+	 * @return an instance of {@link CreateNetworkViewTaskFactory}.
+	 */
+	CreateNetworkViewTaskFactory get_CreateNetworkViewTaskFactory();
+
+	/**
+	 * Returns an instance of {@link ExportNetworkImageTaskFactory}.
+	 * @return an instance of {@link ExportNetworkImageTaskFactory}.
+	 */
+	ExportNetworkImageTaskFactory get_ExportNetworkImageTaskFactory();
+
+	/**
+	 * Returns an instance of {@link ExportNetworkViewTaskFactory}.
+	 * @return an instance of {@link ExportNetworkViewTaskFactory}.
+	 */
+	ExportNetworkViewTaskFactory get_ExportNetworkViewTaskFactory();
+
+	/**
+	 * Returns an instance of {@link ExportCurrentTableTaskFactory}.
+	 * @return an instance of {@link ExportCurrentTableTaskFactory}.
+	 */
+	ExportCurrentTableTaskFactory get_ExportCurrentTableTaskFactory();
+
+	/**
+	 * Returns an instance of {@link ExportVizmapTaskFactory}.
+	 * @return an instance of {@link ExportVizmapTaskFactory}.
+	 */
+	ExportVizmapTaskFactory get_ExportVizmapTaskFactory();
+
+	/**
+	 * Returns an instance of {@link NewSessionTaskFactory}.
+	 * @return an instance of {@link NewSessionTaskFactory}.
+	 */
+	NewSessionTaskFactory get_NewSessionTaskFactory();
+
+	/**
+	 * Returns an instance of {@link OpenSessionTaskFactory}.
+	 * @return an instance of {@link OpenSessionTaskFactory}.
+	 */
+	OpenSessionTaskFactory get_OpenSessionTaskFactory();
+
+	/**
+	 * Returns an instance of {@link saveSessionTaskFactory}.
+	 * @return an instance of {@link saveSessionTaskFactory}.
+	 */
+//	SaveSessionTaskFactory get_saveSessionTaskFactory();
+
+	
+	/**
+	 * Returns an instance of {@link SaveSessionAsTaskFactory}.
+	 * @return an instance of {@link SaveSessionAsTaskFactory}.
+	 */
+	SaveSessionAsTaskFactory get_SaveSessionAsTaskFactory();
+
+	
+	
+	/**
+	 * Returns an instance of {@link ApplyPreferredLayoutTaskFactory}.
+	 * @return an instance of {@link ApplyPreferredLayoutTaskFactory}.
+	 */
+	ApplyPreferredLayoutTaskFactory get_ApplyPreferredLayoutTaskFactory();
+
+	/**
+	 * Returns an instance of {@link DeleteColumnTaskFactory}.
+	 * @return an instance of {@link DeleteColumnTaskFactory}.
+	 */
+	DeleteColumnTaskFactory get_DeleteColumnTaskFactory();
+
+	/**
+	 * Returns an instance of {@link RenameColumnTaskFactory}.
+	 * @return an instance of {@link RenameColumnTaskFactory}.
+	 */
+	RenameColumnTaskFactory get_RenameColumnTaskFactory();
+
+	/**
+	 * Returns an instance of {@link DeleteTableTaskFactory}.
+	 * @return an instance of {@link DeleteTableTaskFactory}.
+	 */
+	DeleteTableTaskFactory get_DeleteTableTaskFactory();
+
+	/**
+	 * Returns an instance of {@link ConnectSelectedNodesTaskFactory}.
+	 * @return an instance of {@link ConnectSelectedNodesTaskFactory}.
+	 */
+	ConnectSelectedNodesTaskFactory get_ConnectSelectedNodesTaskFactory();
+
+//	/**
+//	 * Returns an instance of {@link GroupNodesTaskFactory}.
+//	 * @return an instance of {@link GroupNodesTaskFactory}.
+//	 */
+//	GroupNodesTaskFactory get_GroupNodesTaskFactory();
+//
+//	/**
+//	 * Returns an instance of {@link UnGroupTaskFactory}.
+//	 * @return an instance of {@link UnGroupTaskFactory}.
+//	 */
+//	UnGroupTaskFactory get_UnGroupTaskFactory();
+//
+//	/**
+//	 * Returns an instance of {@link 	CollapseGroupTaskFactory}.
+//	 * @return an instance of {@link 	CollapseGroupTaskFactory}.
+//	 */
+//	CollapseGroupTaskFactory get_CollapseGroupTaskFactory();
+//
+//	/**
+//	 * Returns an instance of {@link ExpandGroupTaskFactory}.
+//	 * @return an instance of {@link ExpandGroupTaskFactory}.
+//	 */
+//	ExpandGroupTaskFactory get_ExpandGroupTaskFactory();
+//
+//	/**
+//	 * Returns an instance of {@link UnGroupNodesTaskFactory}.
+//	 * @return an instance of {@link UnGroupNodesTaskFactory}.
+//	 */
+//	UnGroupNodesTaskFactory get_UnGroupNodesTaskFactory();
+
+	/**
+	 * Returns an instance of {@link MapNetworkAttrTaskFactory}.
+	 * @return an instance of {@link MapNetworkAttrTaskFactory}.
+	 */
+	MapNetworkAttrTaskFactory get_MapNetworkAttrTaskFactory();
+
 }
