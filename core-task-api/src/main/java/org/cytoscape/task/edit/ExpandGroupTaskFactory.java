b6446dc35630ce267fff61ac32ea5ea24d6e01e3
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-26 19:35:13 +0000

    Added group task factories.

---
+++ core-task-api/src/main/java/org/cytoscape/task/edit/ExpandGroupTaskFactory.java
@@ -0,0 +1,11 @@
+package org.cytoscape.task.edit;
+
+import org.cytoscape.task.NodeViewTaskFactory;
+
+/**
+ * This interface provides a task iterator expanding a group from a node view.
+ * @CyAPI.Api.Interface
+ */
+public interface ExpandGroupTaskFactory extends NodeViewTaskFactory {
+
+}
---
+++ core-task-api/src/main/java/org/cytoscape/task/edit/UnGroupNodesTaskFactory.java
@@ -0,0 +1,11 @@
+package org.cytoscape.task.edit;
+
+import org.cytoscape.task.NodeViewTaskFactory;
+
+/**
+ * This interface provides a task iterator ungrouping a selection of nodes from a node view.
+ * @CyAPI.Api.Interface
+ */
+public interface UnGroupNodesTaskFactory extends NodeViewTaskFactory {
+
+}
