c2591a3e57f39c87bf2463f915cf8f4945a5c5b4
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-06-01 22:36:45 +0000

    added command-executor-api, updated the rest of the codebase to use it, and made various tweaks to headless-distribution

---
+++ command-executor-api/src/main/java/org/cytoscape/command/CommandExecutorTaskFactory.java
@@ -0,0 +1,36 @@
+
+package org.cytoscape.command;
+
+import org.cytoscape.work.TaskIterator;
+import java.io.File; 
+import java.util.List;
+
+public interface CommandExecutorTaskFactory {
+
+	/**
+	 * Creates a task iterator that will execute the specified
+	 * commands.
+	 * @param commandFile A file containing a one command string per line,
+	 * blank lines, or comment lines beginning with the '#' character. 
+	 * @return A task iterator that will execute the specified commands.
+	 */
+	TaskIterator createTaskIterator(File commandFile);	
+
+	/**
+	 * Creates a task iterator that will execute the specified
+	 * commands.
+	 * @param commands One or more strings where each string represents
+	 * one command to execute.
+	 * @return A task iterator that will execute the specified commands.
+	 */
+	TaskIterator createTaskIterator(String ... commands);	
+
+	/**
+	 * Creates a task iterator that will execute the specified
+	 * commands.
+	 * @param commands One or more strings where each string represents
+	 * one command to execute.
+	 * @return A task iterator that will execute the specified commands.
+	 */
+	TaskIterator createTaskIterator(List<String> commands);	
+}
