0c390c399b53ec67acedd0d418f904371539649e
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-06-05 21:48:44 +0000

    Confusing names had been modified (from attr to column).  No functional/API changes.

--- vizmap-api/src/main/java/org/cytoscape/view/vizmap/mappings/AbstractVisualMappingFunction.java
+++ vizmap-api/src/main/java/org/cytoscape/view/vizmap/mappings/AbstractVisualMappingFunction.java
@@ -5,18 +5,21 @@ import org.cytoscape.view.model.VisualProperty;
 import org.cytoscape.view.vizmap.VisualMappingFunction;
 
 /**
- * A base class for visual mapping functions where all fields are immutable. 
- * @param <K> Generic type of the attribute mapped.
- * @param <V> Generic type of the {@link VisualProperty} used in this mapping.
+ * A base class for visual mapping functions where all fields are immutable.
+ * 
+ * @param <K>
+ *            Generic type of the attribute mapped.
+ * @param <V>
+ *            Generic type of the {@link VisualProperty} used in this mapping.
  * @CyAPI.Abstract.Class
  */
 public abstract class AbstractVisualMappingFunction<K, V> implements VisualMappingFunction<K, V> {
 
-	/** Mapping attribute name.*/
-	protected final String attrName;
-	
+	/** Mapping attribute name. */
+	protected final String colunName;
+
 	/** Type of attribute. */
-	protected final Class<K> attrType;
+	protected final Class<K> columnType;
 
 	/** The table used for this mapping. */
 	protected final CyTable table;
@@ -24,39 +27,42 @@ public abstract class AbstractVisualMappingFunction<K, V> implements VisualMappi
 	/** Visual Property used in this mapping. */
 	protected final VisualProperty<V> vp;
 
-	
 	/**
 	 * Constructs this AbstractVisualMappingFunction.
-	 * @param attrName Mapping attribute column name.
-	 * @param attrType Type of attribute column.
-	 * @param table The table used to find the column. 
-	 * @param vp Visual Property used in this mapping.
+	 * 
+	 * @param columnName
+	 *            Mapping attribute column name.
+	 * @param columnType
+	 *            Type of attribute column.
+	 * @param table
+	 *            The table used to find the column.
+	 * @param vp
+	 *            Visual Property used in this mapping.
 	 */
-	public AbstractVisualMappingFunction(final String attrName, final Class<K> attrType, final CyTable table,
+	public AbstractVisualMappingFunction(final String columnName, final Class<K> columnType, final CyTable table,
 			final VisualProperty<V> vp) {
-		this.attrType = attrType;
-		this.attrName = attrName;
+		this.columnType = columnType;
+		this.colunName = columnName;
 		this.vp = vp;
 		this.table = table;
 	}
 
-	@Override 
+	@Override
 	public String getMappingColumnName() {
-		return attrName;
+		return colunName;
 	}
 
-	
-	@Override 
+	@Override
 	public Class<K> getMappingColumnType() {
-		return attrType;
+		return columnType;
 	}
 
-	@Override 
+	@Override
 	public VisualProperty<V> getVisualProperty() {
 		return vp;
 	}
-	
-	@Override 
+
+	@Override
 	public CyTable getMappingTable() {
 		return table;
 	}
