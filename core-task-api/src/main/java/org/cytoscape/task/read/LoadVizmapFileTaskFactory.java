84c225709df2eda62376b6763838d996d07a3c25
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-06-06 23:14:35 +0000

    Add a new method

--- core-task-api/src/main/java/org/cytoscape/task/read/LoadVizmapFileTaskFactory.java
+++ core-task-api/src/main/java/org/cytoscape/task/read/LoadVizmapFileTaskFactory.java
@@ -7,6 +7,7 @@ import org.cytoscape.work.TaskIterator;
 
 import java.util.Set;
 import java.io.File;
+import java.io.InputStream;
 
 /**
  * An interface for loading styles from files. 
@@ -21,7 +22,14 @@ public interface LoadVizmapFileTaskFactory extends TaskFactory{
 	 */
 	Set<VisualStyle> loadStyles(File f);
 	
-	
+
+	/**
+	 * Return a set of VisualStyle objects read from the specified file. 
+	 * @param is The inputStream containing visual styles to be read.
+	 * @return a set of VisualStyle objects read from the specified file. 
+	 */
+	Set<VisualStyle> loadStyles(InputStream is);
+
 	/**
 	 * Creates a task iterator for loading files into visual styles.
 	 * The created task runs synchronously in the current thread and does not
