97f742ffaf9c5fd247d03b5f1bc42f0e3d17677a
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-07-18 02:10:47 +0000

    refs #1275 Basic features had been implemented.  Still need to create nicer Visual Style Builder.

---
+++ core-task-api/src/main/java/org/cytoscape/task/analyze/AnalyzeNetworkCollectionTaskFactory.java
@@ -0,0 +1,11 @@
+package org.cytoscape.task.analyze;
+
+import org.cytoscape.task.NetworkCollectionTaskFactory;
+
+/**
+ * Provides a TaskIterator to analyze a collection of networks.
+ *
+ */
+public interface AnalyzeNetworkCollectionTaskFactory extends NetworkCollectionTaskFactory {
+
+}
--- core-task-api/src/main/java/org/cytoscape/task/read/LoadNetworkURLTaskFactory.java
+++ core-task-api/src/main/java/org/cytoscape/task/read/LoadNetworkURLTaskFactory.java
@@ -1,23 +1,24 @@
 package org.cytoscape.task.read;
 
 import java.net.URL;
-import java.util.Set;
 
-import org.cytoscape.model.CyNetwork;
-import org.cytoscape.work.AbstractTaskFactory;
 import org.cytoscape.work.TaskFactory;
 import org.cytoscape.work.TaskIterator;
 
 /**
  * This interface provides a task iterator for loading a URL into a network.
+ * 
  * @CyAPI.Api.Interface
  */
-public interface LoadNetworkURLTaskFactory extends TaskFactory{
-	
+public interface LoadNetworkURLTaskFactory extends TaskFactory {
+
 	/**
-	 * Creates a task iterator for loading a URL into a network. The created task runs
-	 * synchronously in the current thread and does not create a task monitor.
-	 * @param url the URL for loading into the network.
+	 * Creates a task iterator for loading a URL into a network. The created
+	 * task runs synchronously in the current thread and does not create a task
+	 * monitor.
+	 * 
+	 * @param url
+	 *            the URL for loading into the network.
 	 * @return a task iterator of type {@link TaskIterator}.
 	 */
 	TaskIterator loadCyNetworks(final URL url);
