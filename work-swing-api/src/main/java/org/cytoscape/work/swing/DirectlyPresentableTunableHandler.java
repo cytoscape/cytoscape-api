4dbadbb9d50219ceed78faf3a62cd00840aea9f3
rozagh <rozagh@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-07-26 22:42:58 +0000

    fixes #1307 A new method is added to the DirectlyPresentableTunableHandler which checks if the tunable has been forced to set directly. This way, we can prevent single tunables in tasks that needs to be added to the parent panel.
    In BooleanHandler, a new param value named ForceSetDirectly is checked, but in FileHandler the check always returns true.

--- work-swing-api/src/main/java/org/cytoscape/work/swing/DirectlyPresentableTunableHandler.java
+++ work-swing-api/src/main/java/org/cytoscape/work/swing/DirectlyPresentableTunableHandler.java
@@ -10,4 +10,15 @@ public interface DirectlyPresentableTunableHandler {
 	 */
 	boolean setTunableDirectly(Window possibleParent);
 	
+	/**
+	 * If this method returns true, the boolean will be presented
+	 * to set directly from GUI without being a part of a parent panel.
+	 * This method can be controlled by the param field available in
+	 * tunables. For instance, in Boolean tunable if parameter
+	 * "ForceSetDirectly"="true" is defined, the tunable will be
+	 * set directly.  
+	 * @return
+	 */
+	boolean isForcedToSetDirectly();
+	
 }
