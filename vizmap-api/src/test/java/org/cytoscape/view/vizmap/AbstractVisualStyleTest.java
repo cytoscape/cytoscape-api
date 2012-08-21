2d14373ab27164d408b8554abb452c5cea78d7d3
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-08-21 00:02:44 +0000

    Some of the tests are modified.

--- vizmap-api/src/test/java/org/cytoscape/view/vizmap/AbstractVisualStyleTest.java
+++ vizmap-api/src/test/java/org/cytoscape/view/vizmap/AbstractVisualStyleTest.java
@@ -27,7 +27,6 @@ public abstract class AbstractVisualStyleTest {
 	
 	protected static final String attrName = "Sample attr 1";
 	
-	
 	protected static final Color RED1 = new Color(200, 0, 0);
 	protected static final Color GREEN1 = new Color(0, 200, 0);
 	protected static final Color RED2 = new Color(100, 0, 0);
@@ -51,12 +50,12 @@ public abstract class AbstractVisualStyleTest {
 	
 	@Test
 	public void testVisualStyle() {
+		// Make sure all required o
 		assertNotNull(style);
 		assertNotNull(originalTitle);
 		assertNotNull(newTitle);
 		assertNotNull(network);
 		assertNotNull(networkView);
-//		assertNotNull(colorMapping1);
 		assertNotNull(networkView.getNodeView(node1));
 		assertNotNull(networkView.getNodeView(node2));
 		assertNotNull(networkView.getNodeView(node3));
@@ -70,18 +69,13 @@ public abstract class AbstractVisualStyleTest {
 		style.setTitle(newTitle);
 		assertEquals(newTitle, style.getTitle());
 		
-//		style.addVisualMappingFunction(colorMapping1);
-//		style.addVisualMappingFunction(colorMapping2);
-		style.setDefaultValue(BasicVisualLexicon.NODE_PAINT, Color.BLACK);
-		style.setDefaultValue(BasicVisualLexicon.NODE_FILL_COLOR, Color.PINK);
-		
 		final Paint defaultNodeColor = BasicVisualLexicon.NODE_FILL_COLOR.getDefault();
 		final Paint defaultNodePaint = BasicVisualLexicon.NODE_PAINT.getDefault();
 		final View<CyNode> nodeView1 = networkView.getNodeView(node1);
 		final View<CyNode> nodeView2 = networkView.getNodeView(node2);
 		final View<CyNode> nodeView3 = networkView.getNodeView(node3);
 		
-		// Before apply call, all node views should have same color (property default).
+		// Before apply call, all node views should have same color (Visual Property default).
 		assertEquals(defaultNodeColor, nodeView1.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));
 		assertEquals(defaultNodeColor, nodeView2.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));
 		assertEquals(defaultNodeColor, nodeView3.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));
@@ -89,28 +83,39 @@ public abstract class AbstractVisualStyleTest {
 		assertEquals(defaultNodePaint, nodeView2.getVisualProperty(BasicVisualLexicon.NODE_PAINT));
 		assertEquals(defaultNodePaint, nodeView3.getVisualProperty(BasicVisualLexicon.NODE_PAINT));
 		
-		// Two existing mappings.
-//		assertEquals(2, style.getAllVisualMappingFunctions().size());
+		// Set some default values for the VS
+		style.setDefaultValue(BasicVisualLexicon.NODE_FILL_COLOR, Color.PINK);
+		style.setDefaultValue(BasicVisualLexicon.NODE_WIDTH, 60d);
+		style.setDefaultValue(BasicVisualLexicon.NODE_BORDER_WIDTH, 7d);
 		
-		// Apply mappings.
+		assertEquals(Color.PINK, style.getDefaultValue(BasicVisualLexicon.NODE_FILL_COLOR));
+		assertEquals(Double.valueOf(60d), style.getDefaultValue(BasicVisualLexicon.NODE_WIDTH));
+		assertEquals(Double.valueOf(7d), style.getDefaultValue(BasicVisualLexicon.NODE_BORDER_WIDTH));
+		
+		// Apply default values.
 		style.apply(networkView);
 		
-		// Check defaults
-		assertEquals(Color.PINK, style.getDefaultValue(BasicVisualLexicon.NODE_FILL_COLOR));
-		assertEquals(Color.BLACK, style.getDefaultValue(BasicVisualLexicon.NODE_PAINT));
+		// Check defaults are correct
+		assertEquals(Color.PINK, nodeView1.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));
+		assertEquals(Double.valueOf(60d), nodeView1.getVisualProperty(BasicVisualLexicon.NODE_WIDTH));
+		assertEquals(Double.valueOf(7d), nodeView1.getVisualProperty(BasicVisualLexicon.NODE_BORDER_WIDTH));
 		
-		// Check results.
-//		assertEquals(RED1, nodeView1.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR)); // TODO
-//		assertEquals(RED1, nodeView2.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));
+		// Check others
+		assertEquals(Color.PINK, nodeView2.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));
+		assertEquals(Color.PINK, nodeView3.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));
 
 		// Apply to individual values
 		style.setDefaultValue(BasicVisualLexicon.NODE_FILL_COLOR, Color.YELLOW);
 		
 		final CyRow row = networkView.getModel().getRow(nodeView3.getModel());
+		
+		// Apply only to node 3.
 		style.apply(row, nodeView3);
-//		assertEquals(RED1, nodeView1.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR)); // TODO
-//		assertEquals(RED1, nodeView2.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));
-		//assertEquals(Color.YELLOW, nodeView3.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));		
+		
+		// Make sure only node 3 has different color.
+		assertEquals(Color.PINK, nodeView1.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));
+		assertEquals(Color.PINK, nodeView2.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));
+		assertEquals(Color.YELLOW, nodeView3.getVisualProperty(BasicVisualLexicon.NODE_FILL_COLOR));
 	}
 	
 	@Test
