b5562408a62ba40b6918c09c1317eb1e1f1be656
scooter <scooter@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-09-14 21:18:15 +0000

    Added methods to support the idea of a prefix for a CyCustomGraphicsFactory and some support for fetching CustomGraphics data from a URL

--- custom-graphics-api/src/main/java/org/cytoscape/view/presentation/customgraphics/CyCustomGraphicsFactory.java
+++ custom-graphics-api/src/main/java/org/cytoscape/view/presentation/customgraphics/CyCustomGraphicsFactory.java
@@ -1,6 +1,7 @@
 package org.cytoscape.view.presentation.customgraphics;
 
 import java.awt.Image;
+import java.net.URL;
 import java.util.List;
 
 
@@ -12,6 +13,34 @@ import java.util.List;
  *
  */
 public interface CyCustomGraphicsFactory<T extends CustomGraphicLayer> {
+	/**
+ 	 * Return the prefix to delineate this custom graphics factory.  This
+ 	 * is used by the passthrough mapping logic to figure out if a
+ 	 * given String value should be mapped to this factory.
+ 	 *
+ 	 * @return the prefix for this CyCustomGraphics
+ 	 */
+	public String getPrefix();
+
+	/**
+ 	 * Given a mime type, return true if this CyCustomGraphicsFactory can
+ 	 * utilize this type of data.  If this factory doesn't support URL
+ 	 * resources a tall, this should just always return false.
+ 	 *
+ 	 * @param mimeType the mime type string
+ 	 * @return true if this mimeType is supported, false otherwise
+ 	 */
+	public boolean supportsMime(String mimeType);
+
+	/**
+ 	 * Get a new instance of the CyCustomGraphics based on pulling the data
+ 	 * from a URL.
+ 	 *
+ 	 * @param url the url that points to the CyCustomGraphics data
+ 	 * @return the new instance
+ 	 */
+	public CyCustomGraphics<T> getInstance(URL url); 
+
 	/**
  	 * Get a new instance of the CyCustomGraphics.  The string argument may
  	 * be used by some implementations to create the initial graphics objects.
