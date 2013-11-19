af12b0ba8bc13c78a4067d537dd9e2ad31862c31
Jason Montojo <jrrmzz@gmail.com>
2013-11-19 14:50:58 -0500

    References #2314: Added access restriction modifiers to AbstractTransformer to restrict what becomes API.  Added documentation.
    Change-Id: I764e986addee7ccff666a8e8889ffa0a921df705

--- filter-api/src/main/java/org/cytoscape/filter/model/AbstractTransformer.java
+++ filter-api/src/main/java/org/cytoscape/filter/model/AbstractTransformer.java
@@ -3,15 +3,25 @@ package org.cytoscape.filter.model;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+/**
+ * A convenience base class for Transformers.  This class provides a thread-safe
+ * implementation of listener registration, removal and notification.
+ * 
+ * @param <C> The context type of the elements this transformer operates on.
+ * @param <E> The element type this transformer operates on.
+ */
 public abstract class AbstractTransformer<C, E> implements Transformer<C, E> {
-	List<TransformerListener> listeners;
+	private List<TransformerListener> listeners;
 	
+	/**
+	 * Creates a new AbstractTransformer.
+	 */
 	public AbstractTransformer() {
 		listeners = new CopyOnWriteArrayList<TransformerListener>();
 	}
 	
 	@Override
-	public void addListener(TransformerListener listener) {
+	public final void addListener(TransformerListener listener) {
 		if (listeners.contains(listener)) {
 			return;
 		}
@@ -19,11 +29,15 @@ public abstract class AbstractTransformer<C, E> implements Transformer<C, E> {
 	}
 	
 	@Override
-	public void removeListener(TransformerListener listener) {
+	public final void removeListener(TransformerListener listener) {
 		listeners.remove(listener);
 	}
 	
-	protected void notifyListeners() {
+	/**
+	 * Notifies any registered listeners that this transformer's settings
+	 * have changed.
+	 */
+	protected final void notifyListeners() {
 		for (TransformerListener listener : listeners) {
 			listener.handleSettingsChanged();
 		}
