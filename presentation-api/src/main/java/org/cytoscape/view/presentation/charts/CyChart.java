0b235378c91dbdce76c0576c13359301c751c3eb
Christian Lopes <chrtannus@gmail.com>
2014-04-28 16:50:11 -0400

    Refs #2626: Adds CyChart and other related interfaces.

---
+++ presentation-api/src/main/java/org/cytoscape/view/presentation/charts/CyChart.java
@@ -0,0 +1,9 @@
+package org.cytoscape.view.presentation.charts;
+
+import org.cytoscape.view.presentation.customgraphics.CustomGraphicLayer;
+import org.cytoscape.view.presentation.customgraphics.CyCustomGraphics;
+
+public interface CyChart<T extends CustomGraphicLayer> extends CyCustomGraphics<T> {
+
+	
+}
