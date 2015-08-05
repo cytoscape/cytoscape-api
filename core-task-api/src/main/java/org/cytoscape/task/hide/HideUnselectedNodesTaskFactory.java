e01e0a682931bf08a4141d6969e3911ec58aedcd
Christian Lopes <chrtannus@gmail.com>
2015-08-04 21:56:51 -0400

    Refs #3217: Adds 3 marker interfaces for factories of tasks that hide unselected nodes, edges or both.

---
+++ core-task-api/src/main/java/org/cytoscape/task/hide/HideUnselectedNodesTaskFactory.java
@@ -0,0 +1,36 @@
+package org.cytoscape.task.hide;
+
+/*
+ * #%L
+ * Cytoscape Core Task API (core-task-api)
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2006 - 2013 The Cytoscape Consortium
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+import org.cytoscape.task.NetworkViewTaskFactory;
+
+/**
+ * This interface provides a task iterator for hiding unselected nodes.
+ * @CyAPI.Api.Interface
+ * @CyAPI.InModule core-task-api
+ */
+public interface HideUnselectedNodesTaskFactory extends NetworkViewTaskFactory {
+
+}
