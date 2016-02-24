c7faae2bfeba1c87fb95ec3a60e3eeebf76ac81d
Scooter Morris <scooter@cgl.ucsf.edu>
2016-02-24 11:57:44 -0800

    Deprecate TaskStatusPanelFactory, which was only used for the TaskStatus icon and replace with StatusBarPanelFactory so that it can be used for more buttons on the status bar.  Also added hourglass icons to the IconManager

--- work-swing-api/src/main/java/org/cytoscape/work/swing/TaskStatusPanelFactory.java
+++ work-swing-api/src/main/java/org/cytoscape/work/swing/TaskStatusPanelFactory.java
@@ -6,8 +6,12 @@ import javax.swing.JPanel;
  * Creates a {@code JPanel} that summarizes
  * currently executing tasks.
  * This panel is typically shown at the bottom
- * of the Cytoscape Desktop.
+ * of the Cytoscape Desktop.</br>
+ * NOTE: implementations of this are no longer
+ * used within the core.
+ * @deprecated
  */
+@Deprecated
 public interface TaskStatusPanelFactory {
     public JPanel createTaskStatusPanel();
 }
