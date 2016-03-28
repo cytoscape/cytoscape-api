22396aee336ca82008d5859d40c528677692e5b0
Scooter Morris <scooter@cgl.ucsf.edu>
2016-03-28 10:08:32 -0700

    Some fixes to SUIDUtil and a slight API change that allows Apps to clean up columns they no longer need

--- jobs-api/src/main/java/org/cytoscape/jobs/SUIDUtil.java
+++ jobs-api/src/main/java/org/cytoscape/jobs/SUIDUtil.java
@@ -92,15 +92,18 @@ public class SUIDUtil {
 	 * @param job the {@link CyJob} we're restoring
 	 * @param netManager the {@link CyNetworkManager} to get the list of networks
 	 * @param networkSUID the SUIDof the network we're restoring
+	 * @param clear if true, remove the column for this job
 	 * @return the restored network or null if we can't find it
 	 */
-	static public CyNetwork restoreNetwork(CyJob job, CyNetworkManager netManager, Long networkSUID) {
+	static public CyNetwork restoreNetwork(CyJob job, CyNetworkManager netManager, Long networkSUID, boolean clear) {
 		String columnName = getColumnName(job);
 		for (CyNetwork network: netManager.getNetworkSet()) {
 			CyTable table = network.getTable(CyNetwork.class, CyNetwork.HIDDEN_ATTRS);
 			if (table.getColumn(columnName) == null)
 				continue;
 			if (table.getRow(network.getSUID()).get(columnName, Long.class).equals(networkSUID)) {
+				if (clear)
+					table.deleteColumn(columnName);
 				return network;
 			}
 		}
@@ -116,9 +119,10 @@ public class SUIDUtil {
 	 * a method to get the network
 	 * @param oldIds the list of old SUIDs that were sent to the remote server and saved
 	 * in the session
+	 * @param clear if true, remove the columns for this job
 	 * @return a map that relates the original id to the Cytoscape object
 	 */
-	static public Map<Long,CyIdentifiable> restoreSUIDs(CyJob job, CyNetwork network, List<Long> oldIds) {
+	static public Map<Long,CyIdentifiable> restoreSUIDs(CyJob job, CyNetwork network, List<Long> oldIds, boolean clear) {
 		Map<Long,CyIdentifiable> objMap = new HashMap<>();
 		Set<Long> suidSet = new HashSet<Long>(oldIds);
 
@@ -130,26 +134,33 @@ public class SUIDUtil {
 			Long id = network.getRow(network).get(columnName, Long.class);
 			if (suidSet.contains(id))
 				objMap.put(id,network);
+			if (clear)
+				networkTable.deleteColumn(columnName);
 		}
 
 		CyTable nodeTable = network.getTable(CyNode.class, CyNetwork.HIDDEN_ATTRS);
 		if (nodeTable.getColumn(columnName) != null) {
 			for (CyNode node: network.getNodeList()) {
-				Long id = network.getRow(node).get(columnName, Long.class);
+				Long id = nodeTable.getRow(node.getSUID()).get(columnName, Long.class);
 				if (id == null) continue;
-				if (suidSet.contains(id))
+				if (suidSet.contains(id)) {
 					objMap.put(id,node);
+				}
 			}
+			if (clear)
+				nodeTable.deleteColumn(columnName);
 		}
 
 		CyTable edgeTable = network.getTable(CyEdge.class, CyNetwork.HIDDEN_ATTRS);
 		if (edgeTable.getColumn(columnName) != null) {
 			for (CyEdge edge: network.getEdgeList()) {
-				Long id = network.getRow(edge).get(columnName, Long.class);
+				Long id = edgeTable.getRow(edge.getSUID()).get(columnName, Long.class);
 				if (id == null) continue;
 				if (suidSet.contains(id))
 					objMap.put(id,edge);
 			}
+			if (clear)
+				edgeTable.deleteColumn(columnName);
 		}
 		return objMap;
 	}
