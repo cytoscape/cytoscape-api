c6bd4db6e666f1087573a75279ff7e843d0ca19b
Christian Lopes <chrtannus@gmail.com>
2016-06-07 15:07:57 -0300

    Fixes Help button text/icon and uses Font Awesome icon on Windows and Nimbus. Minor refactoring.

--- swing-util-api/src/main/java/org/cytoscape/util/swing/LookAndFeelUtil.java
+++ swing-util-api/src/main/java/org/cytoscape/util/swing/LookAndFeelUtil.java
@@ -1,12 +1,39 @@
 package org.cytoscape.util.swing;
 
+import java.awt.Color;
+import java.awt.Desktop;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout.ParallelGroup;
+import javax.swing.GroupLayout.SequentialGroup;
+import javax.swing.InputMap;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+
 /*
  * #%L
  * Cytoscape Swing Utility API (swing-util-api)
  * $Id:$
  * $HeadURL:$
  * %%
- * Copyright (C) 2006 - 2013 The Cytoscape Consortium
+ * Copyright (C) 2006 - 2016 The Cytoscape Consortium
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as 
@@ -24,36 +51,6 @@ package org.cytoscape.util.swing;
  * #L%
  */
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Desktop;
-import java.awt.event.KeyEvent;
-
-import javax.swing.Action;
-import java.awt.event.ActionEvent;
-import java.net.URI;
-import java.net.URL;
-import javax.swing.AbstractAction;
-import javax.swing.BorderFactory;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.GroupLayout.ParallelGroup;
-import javax.swing.GroupLayout.SequentialGroup;
-import javax.swing.InputMap;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import javax.swing.JRootPane;
-import javax.swing.KeyStroke;
-import javax.swing.UIManager;
-import javax.swing.border.Border;
-import javax.swing.border.TitledBorder;
-import javax.swing.BoxLayout;
-import javax.swing.Box;
-import org.cytoscape.util.swing.IconManager;
-import org.cytoscape.util.swing.OpenBrowser;
-
-
 /**
  * Class that provides useful methods to help create standard and consistent UI on the current Look and Feel and OS.
  * 
@@ -63,6 +60,19 @@ import org.cytoscape.util.swing.OpenBrowser;
 public final class LookAndFeelUtil {
 
 	static final float AQUA_TITLED_BORDER_FONT_SIZE = 11.0f;
+	
+	private static Font iconFont;
+
+	static {
+		try {
+			iconFont = Font.createFont(Font.TRUETYPE_FONT,
+					LookAndFeelUtil.class.getResourceAsStream("/fonts/fontawesome-webfont.ttf"));
+		} catch (FontFormatException e) {
+			throw new RuntimeException();
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
 	
 	/**
 	 * @return true if the current Look and Feel is "Aqua", usually available on Mac OS X only.
@@ -180,86 +190,14 @@ public final class LookAndFeelUtil {
 		return createOkCancelPanel(okBtn, cancelBtn, new JComponent[0]);
 	}
 	
-	//-----------------------------------------------------------------
 	/**
 	 * Use this method to create OK, Cancel and Help
-	 	 * @param helpStr can be a full url, a lookup, or a help page name
-**/
+	 * @param helpStr can be a full url, a lookup, or a help page name
+	 */
 	public static JPanel createOkCancelPanel(final JButton okBtn, final JButton cancelBtn, String helpStr) {
 		return createOkCancelPanel(okBtn, cancelBtn, helpStr, new JComponent[0]);
 	}
 	
-	//FIXME         don't hardcode version tag!
-	private static String HELP_PREFIX = "http://manual.cytoscape.org/en/3.4.0/";
-	private static    String HELP_SUFFIX = ".html";
-
-	private static URI helpStrToURI(String helpStr)
-	{
-	  	try
-	  	{
-	        return new URL(helpStrToURL(helpStr)).toURI();
-	  	} catch (Exception e) {  System.out.println(" helpStrToURI  Exception: " + e.getMessage());}
-         
-        return null; 
-        
-	}
-	
-	private static String helpStrToURL(String rawString)
-	{
-
-	    if (rawString.startsWith("http")) return rawString;
-	    return HELP_PREFIX + rawString + HELP_SUFFIX;
-
-	}
-
-	
-	private static void launch(String helpStr)       
-	{
-//        System.out.println("launch: " + helpStr);
-        try{
-//           System.out.println("try: " + helpStrToURL(helpStr));
-            URI uri = helpStrToURI(helpStr);
-            if (uri != null)            	
-                Desktop.getDesktop().browse(uri);
-//             else    System.out.println("no uri " );
-       }
-        catch(Exception ex){
-               System.out.println("Exception: " + ex.getMessage());
-        }   
-     }  
-	
-
-	public static JButton makeHelpButton(String helpStr)
-	{
-
-        JButton helpButton = new JButton(new AbstractAction("?") {
-            @Override
-            public void actionPerformed(ActionEvent e) {    launch(helpStr);    }});
-        helpButton.setPreferredSize(new Dimension(24,24));  
-//         helpButton.setText(IconManager.ICON_QUESTION_CIRCLE);
-//         helpButton.setFont(IconManager.getIconFont(14.0f)); 
-        if (isMac()) 
-        	helpButton.putClientProperty("JButton.buttonType", "help");
-        return helpButton;
-	}
-	
-	/**
-	 * Use this method to include a help button in the bottom left corner.
-	 	 * @param helpStr can be a full url, a lookup, or a help page name
-
-**/
-	public static JPanel createOkCancelPanel(final JButton okBtn, final JButton cancelBtn,
-			String helpStr,         //new #3534   http://code.cytoscape.org/redmine/issues/3534
-			JComponent... otherComponents) {
- 
-        JPanel parentPanel = new JPanel();
-        parentPanel.setLayout(new BoxLayout(parentPanel, BoxLayout.LINE_AXIS));
-        parentPanel.add(makeHelpButton(helpStr));
-        parentPanel.add(Box.createHorizontalGlue());
-        parentPanel.add(createOkCancelPanel( okBtn, cancelBtn, otherComponents));
-        return parentPanel; 
-    }
-
 	/**
 	 * Use this method to create a standard Cytoscape panel (usually added to the bottom of a dialog)
 	 * that contains an "OK" and/or a "Cancel" button, as well as other extra components.
@@ -272,6 +210,15 @@ public final class LookAndFeelUtil {
 	 */
 	public static JPanel createOkCancelPanel(final JButton okBtn, final JButton cancelBtn,
 			JComponent... otherComponents) {
+		return createOkCancelPanel(okBtn, cancelBtn, null, otherComponents);
+	}
+	
+	/**
+	 * Use this method to include a help button in the bottom left corner.
+	 * @param helpStr can be a full url, a lookup, or a help page name
+	 */
+	public static JPanel createOkCancelPanel(final JButton okBtn, final JButton cancelBtn, String helpStr,
+			JComponent... otherComponents) {
 		final JPanel panel = new JPanel();
 		
 		final GroupLayout layout = new GroupLayout(panel);
@@ -281,6 +228,12 @@ public final class LookAndFeelUtil {
 		
 		final SequentialGroup hg = layout.createSequentialGroup();
 		final ParallelGroup vg = layout.createParallelGroup(Alignment.CENTER, false);
+		
+		if (helpStr != null && !helpStr.trim().isEmpty()) {
+			final JButton helpButton = createHelpButton(helpStr);
+			hg.addComponent(helpButton);
+			vg.addComponent(helpButton);
+		}
 		
 		if (otherComponents != null) {
 			for (int i = 0; i < otherComponents.length; i++) {
@@ -309,8 +262,31 @@ public final class LookAndFeelUtil {
 		
 		if (okBtn != null && cancelBtn != null)
 			equalizeSize(okBtn, cancelBtn);
+        
+        return panel; 
+    }
+	
+	public static JButton createHelpButton(final String helpStr) {
+		final JButton helpButton = new JButton();
+		helpButton.setToolTipText("Help...");
+
+		if (isAquaLAF()) {
+			helpButton.putClientProperty("JButton.buttonType", "help");
+		} else {
+			helpButton.setText(IconManager.ICON_QUESTION_CIRCLE);
+			helpButton.setFont(getIconFont(22.0f));
+			helpButton.setBorderPainted(false);
+			helpButton.setContentAreaFilled(false);
+			helpButton.setFocusPainted(false);
+			helpButton.setBorder(BorderFactory.createEmptyBorder());
+			helpButton.setMinimumSize(new Dimension(22, 22));
+		}
 		
-		return panel;
+		helpButton.addActionListener((ActionEvent e) -> {
+			launch(helpStr);
+		});
+		
+		return helpButton;
 	}
 	
 	/**
@@ -392,6 +368,42 @@ public final class LookAndFeelUtil {
 		
 		if (currentSize.width < minSize.width)
 			currentSize.width = minSize.width;
+	}
+	
+	//FIXME         don't hardcode version tag!
+	private static String HELP_PREFIX = "http://manual.cytoscape.org/en/3.4.0/";
+	private static String HELP_SUFFIX = ".html";
+
+	private static URI helpStrToURI(String helpStr) {
+		try {
+			return new URL(helpStrToURL(helpStr)).toURI();
+		} catch (Exception e) {
+			System.out.println(" helpStrToURI  Exception: " + e.getMessage());
+		}
+
+		return null;
+	}
+
+	private static String helpStrToURL(String rawString) {
+		if (rawString.startsWith("http"))
+			return rawString;
+
+		return HELP_PREFIX + rawString + HELP_SUFFIX;
+	}
+
+	private static void launch(String helpStr) {
+		try {
+			URI uri = helpStrToURI(helpStr);
+			
+			if (uri != null)
+				Desktop.getDesktop().browse(uri);
+		} catch (Exception ex) {
+			System.out.println("Exception: " + ex.getMessage());
+		}
+	}
+	
+	private static Font getIconFont(float size) {
+		return iconFont.deriveFont(size);
 	}
 	
 	private LookAndFeelUtil() {
