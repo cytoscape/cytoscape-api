f3975a6d66d737f7d9742079e8d1a65a4abb70b2
Christian Lopes <chrtannus@gmail.com>
2017-05-15 17:36:56 -0400

    Removes RandomImage (moved to impl).

--- swing-application-api/src/main/java/org/cytoscape/application/swing/search/AbstractNetworkSearchTaskFactory.java
+++ swing-application-api/src/main/java/org/cytoscape/application/swing/search/AbstractNetworkSearchTaskFactory.java
@@ -1,14 +1,8 @@
 package org.cytoscape.application.swing.search;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
 import java.net.URL;
-import java.util.Random;
 
 import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 
 import org.cytoscape.work.AbstractTaskFactory;
@@ -22,8 +16,6 @@ import org.cytoscape.work.TaskObserver;
  */
 public abstract class AbstractNetworkSearchTaskFactory extends AbstractTaskFactory implements NetworkSearchTaskFactory {
 	
-	private static final int ICON_SIZE = 32;
-
 	private String query;
 	
 	private final String id;
@@ -49,7 +41,7 @@ public abstract class AbstractNetworkSearchTaskFactory extends AbstractTaskFacto
 		this.id = id;
 		this.name = name;
 		this.description = description;
-		this.icon = icon != null ? icon : new ImageIcon(new RandomImage(ICON_SIZE, ICON_SIZE));
+		this.icon = icon;
 		this.website = website;
 	}
 	
@@ -152,74 +144,5 @@ public abstract class AbstractNetworkSearchTaskFactory extends AbstractTaskFacto
 	@Override
 	public String toString() {
 		return name;
-	}
-	
-	private class RandomImage extends BufferedImage {
-
-		private final int grain = 5;
-		private final int colorRange = 5;
-		
-		public RandomImage(int width, int height) {
-			super(width, height, BufferedImage.TYPE_INT_ARGB);
-			draw();
-		}
-		
-		private void draw() {
-			int w = getWidth();
-			int h = getHeight();
-			
-			Graphics2D g2 = (Graphics2D) getGraphics();
-			g2.setRenderingHints(new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING,
-					RenderingHints.VALUE_TEXT_ANTIALIAS_ON));
-
-			int max = 200, min = 100;
-			
-			Color color = randomColor();
-			int red = color.getRed();
-			int green = color.getGreen();
-			int blue = color.getBlue();
-			
-			g2.setColor(color);
-			
-			double blockout = Math.random();
-			int x = 0, y = 0;
-			
-			for (int i = 0; i < grain; i++) {
-				for (int j = 0; j < grain; j++) {
-					if (blockout < 0.4) {
-						g2.fillRect(x, y, w / grain, h / grain);
-						g2.fillRect(w - x - w / grain, y, w / grain, h / grain);
-						x += w / grain;
-					} else {
-						red -= colorRange;
-						red = Math.min(max, Math.max(red, min));
-						
-						green += colorRange;
-						green = Math.min(max, Math.max(green, min));
-						
-						blue += colorRange;
-						blue = Math.min(max, Math.max(blue, min));
-						
-						g2.setColor(new Color(red, green, blue));
-						x += w / grain;
-					}
-					
-					blockout = Math.random();
-				}
-				
-				y += h / grain;
-				x = 0;
-			}
-		}
-		
-		private Color randomColor() {
-			// Get rainbow, pastel colors
-			Random random = new Random();
-			final float hue = random.nextFloat();
-			final float saturation = 0.9f;// 1.0 for brilliant, 0.0 for dull
-			final float luminance = 1.0f; // 1.0 for brighter, 0.0 for black
-			
-			return Color.getHSBColor(hue, saturation, luminance);
-		}
 	}
 }
