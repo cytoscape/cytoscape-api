892eaf59de4c28750b7e5ec69dde8597d3b2dea6
David Otasek <dotasek.dev@gmail.com>
2017-08-04 11:33:18 -0400

    Added interface for CyModelJSONUtil

---
+++ model-api/src/main/java/org/cytoscape/model/json/CyModelJSONUtil.java
@@ -0,0 +1,30 @@
+package org.cytoscape.model.json;
+
+import java.util.Collection;
+
+import org.cytoscape.model.CyColumn;
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyIdentifiable;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyRow;
+import org.cytoscape.model.CyTable;
+
+public interface CyModelJSONUtil {
+	
+	public String toJson(CyIdentifiable cyIdentifiable);
+
+	public String toJson(Collection<CyIdentifiable> collection);
+	
+	public String toJson(CyNetwork network, CyNode cyNode);
+	
+	public String toJson(CyNetwork network, CyEdge cyEdge);
+	
+	public String toJson(CyNetwork cyNetwork);
+	
+	public String toJson(CyTable cyTable);
+	
+	public String toJson(CyColumn cyColumn);
+	
+	public String toJson(CyRow cyRow);
+}
