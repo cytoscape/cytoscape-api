1c78825d6d81ff7547f7a4165a98b7fcb4227e0b
Christian Lopes <chrtannus@gmail.com>
2018-05-16 18:23:41 -0400

    Refs #4188: Adds API for CyRootNetwork tasks and factories.

---
+++ core-task-api/src/main/java/org/cytoscape/task/AbstractRootNetworkTaskFactory.java
@@ -0,0 +1,45 @@
+package org.cytoscape.task;
+
+import org.cytoscape.model.subnetwork.CyRootNetwork;
+
+/*
+ * #%L
+ * Cytoscape Core Task API (core-task-api)
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2006 - 2018 The Cytoscape Consortium
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+/**
+ * A {@link RootNetworkTaskFactory} implementation that is always ready to produce a TaskIterator.
+ * @CyAPI.Abstract.Class
+ * @CyAPI.InModule core-task-api
+ */
+public abstract class AbstractRootNetworkTaskFactory implements RootNetworkTaskFactory {
+	
+	/**
+	 * Returns true if the supplied network is not null.
+	 * @param network The root-network.
+	 * @return true if the supplied root-network is not null.
+	 */
+	@Override
+	public boolean isReady(CyRootNetwork network) {
+		return network != null;
+	}
+}
