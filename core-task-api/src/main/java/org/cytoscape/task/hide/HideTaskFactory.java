d512a1526f6874344ed2b718b4ae8cca61bed68c
Mike Kucera <mikekucera@gmail.com>
2018-07-11 11:15:14 -0400

    JavaDoc for hide tasks. Refs #3871

--- core-task-api/src/main/java/org/cytoscape/task/hide/HideTaskFactory.java
+++ core-task-api/src/main/java/org/cytoscape/task/hide/HideTaskFactory.java
@@ -7,8 +7,19 @@ import org.cytoscape.model.CyNode;
 import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.work.TaskIterator;
 
+/**
+ * This interface provides a task iterator for hiding the given nodes and edges.
+ * @CyAPI.Api.Interface
+ * @CyAPI.InModule core-task-api
+ */
 public interface HideTaskFactory {
 
+	/**
+	 * Creates a task iterator that will hide the given nodes and edges.
+	 * @param view The CyNetworkView to that contains the nodes and edges
+	 * @param nodes A collection of nodes to hide, may be null.
+	 * @param edges A collection of edges to hide, may be null.
+	 */
 	TaskIterator createTaskIterator(CyNetworkView view, Collection<CyNode> nodes, Collection<CyEdge> edges);
 	
 }
