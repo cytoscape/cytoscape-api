36b38269097e0cd840657cc88c4df6b929d95f97
Mike Kucera <mikekucera@gmail.com>
2020-12-04 11:27:53 -0500

    Add annotations to thumbnail service.

--- presentation-api/src/main/java/org/cytoscape/view/presentation/ThumbnailFactory.java
+++ presentation-api/src/main/java/org/cytoscape/view/presentation/ThumbnailFactory.java
@@ -1,9 +1,11 @@
 package org.cytoscape.view.presentation;
 
 import java.awt.Image;
+import java.util.Collection;
 import java.util.Map;
 
 import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.presentation.annotations.Annotation;
 
 
 
@@ -27,18 +29,19 @@ public interface ThumbnailFactory {
 	public static final String WIDTH = "width";
 	
 	/**
-	 * <p>
-	 * If true the network will be automatically centered inside the image bounds.
-	 * </p><p>
-	 * If false then the network Visual Properties NETWORK_SCALE_FACTOR, 
+	 * <p> If true the network will be automatically centered inside the image bounds.</p>
+	 * <p> If false then the network Visual Properties NETWORK_SCALE_FACTOR, 
 	 * NETWORK_CENTER_X_LOCATION, NETWORK_CENTER_Y_LOCATION will be used to position and scale
-	 * the image. The origin for the image coordinates is at the center of the image.
-	 * </p>
+	 * the image. The origin for the image coordinates is at the center of the image.</p>
 	 * Default: true.
 	 */
 	public static final String FIT_CONTENT = "fitContent";
 
 	
-	Image getThumbnail(CyNetworkView networkView, Map<String,Object> properties);
+	Image getThumbnail(CyNetworkView networkView, Collection<Annotation> annotations, Map<String,Object> properties);
+	
+	default Image getThumbnail(CyNetworkView networkView, Map<String,Object> properties) {
+		return getThumbnail(networkView, null, properties);
+	}
 
 }
