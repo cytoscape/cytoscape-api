70f2199b746e3d8217ffbd2d5172b1fff64b7e6d
dotasek <dotasek.dev@gmail.com>
2021-04-16 13:27:32 -0400

    Cleaner AbstractValidatableTransformer

--- filter-api/src/main/java/org/cytoscape/filter/model/AbstractValidatableTransformer.java
+++ filter-api/src/main/java/org/cytoscape/filter/model/AbstractValidatableTransformer.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
- * A convenience base class for Transformers.  This class provides a thread-safe
+ * A convenience base class for ValidatableTransformers.  This class provides a thread-safe
  * implementation of listener registration, removal and notification.
  * 
  * @param <C> The context type of the elements this {@link Transformer} operates on.
@@ -13,36 +13,12 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * @CyAPI.Abstract.Class
  * @CyAPI.InModule filter-api
  */
-public abstract class AbstractValidatableTransformer<C, E> implements ValidatableTransformer<C, E> {
-	private List<TransformerListener> listeners;
+public abstract class AbstractValidatableTransformer<C, E> extends AbstractTransformer<C,E> implements ValidatableTransformer<C, E> {
 	
 	/**
-	 * Creates a new AbstractTransformer.
+	 * Creates a new AbstractValidatableTransformer.
 	 */
 	public AbstractValidatableTransformer() {
-		listeners = new CopyOnWriteArrayList<TransformerListener>();
-	}
-	
-	@Override
-	public final void addListener(TransformerListener listener) {
-		if (listeners.contains(listener)) {
-			return;
-		}
-		listeners.add(listener);
-	}
-	
-	@Override
-	public final void removeListener(TransformerListener listener) {
-		listeners.remove(listener);
-	}
-	
-	/**
-	 * Notifies any registered listeners that this transformer's settings
-	 * have changed.
-	 */
-	protected final void notifyListeners() {
-		for (TransformerListener listener : listeners) {
-			listener.handleSettingsChanged();
-		}
+		super();
 	}
 }
