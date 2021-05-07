85bf768218ff76f12b2f9354f67bbcd75d192026
dotasek <dotasek.dev@gmail.com>
2021-05-06 20:28:24 -0400

    Update copyright

--- event-api/src/test/java/org/cytoscape/event/AbstractCyEventHelperTest.java
+++ event-api/src/test/java/org/cytoscape/event/AbstractCyEventHelperTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertTrue;
  * $Id:$
  * $HeadURL:$
  * %%
- * Copyright (C) 2008 - 2013 The Cytoscape Consortium
+ * Copyright (C) 2008 - 2021 The Cytoscape Consortium
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as 
