91c5ff80078105049476ed5f34d28a6c5601d0ed
Mike Kucera <mikekucera@gmail.com>
2021-05-21 10:49:43 -0400

    Add cancellation to NetworkSpacialIndex2D.

--- viewmodel-api/src/main/java/org/cytoscape/view/model/spacial/NetworkSpacialIndex2D.java
+++ viewmodel-api/src/main/java/org/cytoscape/view/model/spacial/NetworkSpacialIndex2D.java
@@ -1,18 +1,24 @@
 package org.cytoscape.view.model.spacial;
 
+import java.util.function.BooleanSupplier;
+
 /**
  * A spacial index used to query for locations of Nodes and Edges in a 2D representation of a network.
+ * 
+ * @CyAPI.Spi.Interface
+ * @CyAPI.InModule viewmodel-api
+ * @since 3.9
  */
 public interface NetworkSpacialIndex2D extends SpacialIndex2D<Long> {
 
 	
-	public NodeSpacialIndex2DEnumerator queryAllNodes();
+	public NodeSpacialIndex2DEnumerator queryAllNodes(BooleanSupplier isCancelled);
 	
-	public NodeSpacialIndex2DEnumerator queryOverlapNodes(float xMin, float yMin, float xMax, float yMax);
+	public NodeSpacialIndex2DEnumerator queryOverlapNodes(float xMin, float yMin, float xMax, float yMax, BooleanSupplier isCancelled);
 	
 	
-	public EdgeSpacialIndex2DEnumerator queryAllEdges();
+	public EdgeSpacialIndex2DEnumerator queryAllEdges(BooleanSupplier isCancelled);
 	
-	public EdgeSpacialIndex2DEnumerator queryOverlapEdges(float xMin, float yMin, float xMax, float yMax);
+	public EdgeSpacialIndex2DEnumerator queryOverlapEdges(float xMin, float yMin, float xMax, float yMax, BooleanSupplier isCancelled);
 	
 }
