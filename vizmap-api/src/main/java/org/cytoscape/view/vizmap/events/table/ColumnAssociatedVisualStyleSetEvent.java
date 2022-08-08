727138a7e391a613a558d70ff3e5d83a0ac2f4ec
Mike Kucera <mikekucera@gmail.com>
2022-08-08 15:40:32 -0400

    Simplify event

--- vizmap-api/src/main/java/org/cytoscape/view/vizmap/events/table/ColumnAssociatedVisualStyleSetEvent.java
+++ vizmap-api/src/main/java/org/cytoscape/view/vizmap/events/table/ColumnAssociatedVisualStyleSetEvent.java
@@ -1,46 +1,21 @@
 package org.cytoscape.view.vizmap.events.table;
 
 import org.cytoscape.event.AbstractCyEvent;
-import org.cytoscape.model.CyIdentifiable;
+import org.cytoscape.view.vizmap.StyleAssociation;
 import org.cytoscape.view.vizmap.TableVisualMappingManager;
-import org.cytoscape.view.vizmap.VisualStyle;
 
 public class ColumnAssociatedVisualStyleSetEvent extends AbstractCyEvent<TableVisualMappingManager> {
 
-	private final VisualStyle networkVisualStyle;
-	private final VisualStyle columnVisualStyle; // may be null if VS was removed
-	private final String columnName;
-	private final Class<? extends CyIdentifiable> tableType;
+	private final StyleAssociation association; // The column style field may be null if the visual style was removed.
 	
 	
-	public ColumnAssociatedVisualStyleSetEvent(
-			TableVisualMappingManager source, 
-			VisualStyle networkVisualStyle, 
-			VisualStyle columnVisualStyle, 
-			String columnName, Class<? extends CyIdentifiable> tableType
-	) {
+	public ColumnAssociatedVisualStyleSetEvent(TableVisualMappingManager source, StyleAssociation association) {
 		super(source, ColumnAssociatedVisualStyleSetListener.class);
-		this.networkVisualStyle = networkVisualStyle;
-		this.columnVisualStyle = columnVisualStyle;
-		this.columnName = columnName;
-		this.tableType = tableType;
+		this.association = association;
 	}
-
-
-	public VisualStyle getNetworkVisualStyle() {
-		return networkVisualStyle;
-	}
-
-	public VisualStyle getColumnVisualStyle() {
-		return columnVisualStyle;
-	}
-
-	public String getColumnName() {
-		return columnName;
-	}
-
-	public Class<? extends CyIdentifiable> getTableType() {
-		return tableType;
+	
+	public StyleAssociation getAssociation() {
+		return association;
 	}
 	
 }
