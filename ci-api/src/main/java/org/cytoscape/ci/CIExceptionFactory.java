8fbed90e36f6ab88493fbe56d51a206aedec26d0
Scooter Morris <scooter@cgl.ucsf.edu>
2026-05-02 10:33:31 +0200

    Revert "jdk21 changes"
    This reverts commit 5ec64c3131c293892ddc6210d4189e975fecbb01.

--- ci-api/src/main/java/org/cytoscape/ci/CIExceptionFactory.java
+++ ci-api/src/main/java/org/cytoscape/ci/CIExceptionFactory.java
@@ -1,5 +1,7 @@
 package org.cytoscape.ci;
 
+import javax.ws.rs.WebApplicationException;
+
 import org.cytoscape.ci.model.CIError;
 
 /**
@@ -8,6 +10,6 @@ import org.cytoscape.ci.model.CIError;
  *
  */
 public interface CIExceptionFactory {
-	public RuntimeException getCIException(int httpStatus, CIError[] errors);
-	public <K> RuntimeException getCIException(int httpStatus, K data, CIError[] errors);
+	public WebApplicationException getCIException(int httpStatus, CIError[] errors);
+	public <K> WebApplicationException getCIException(int httpStatus, K data, CIError[] errors);
 }
